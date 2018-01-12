0d38744f247cb7d84a6c8e972b8147b94b9be956
dhywty <clemwek@gmail.com>
2018-01-12 21:50:45 +0300

    Fix material ui missing dependencies.

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import WelcomePage from './components/home/welcome/welcome';
+import MenuAppBar from './components/common/navbar/navbar';
 
 export default () => (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={WelcomePage} />
-    </Switch>
+    <div>
+      <Route component={MenuAppBar} />
+      <Route exact path="/" component={WelcomePage} />
+      <Route exact path="/admin" render={() => <h1>This is the admin page.</h1>} />
+      <Route exact path="/staff" render={() => <h1>This is the staff page.</h1>} />
+    </div>
   </BrowserRouter>
 );
