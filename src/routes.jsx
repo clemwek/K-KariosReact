5a98c2cabb52e6178435e40fcb117f0efca9e532
dhywty <clemwek@gmail.com>
2018-01-13 00:52:00 +0300

    Add admin and staff page

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import WelcomePage from './components/home/welcome/welcome';
 import MenuAppBar from './components/common/navbar/navbar';
+import StaffPage from './components/admin/staff/staff';
 
 export default () => (
   <BrowserRouter>
     <div>
       <Route component={MenuAppBar} />
       <Route exact path="/" component={WelcomePage} />
+      <Route exact path="/staff" component={StaffPage} />
       <Route exact path="/admin" render={() => <h1>This is the admin page.</h1>} />
-      <Route exact path="/staff" render={() => <h1>This is the staff page.</h1>} />
     </div>
   </BrowserRouter>
 );
