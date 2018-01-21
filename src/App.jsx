f47dca26b5aa21cdf0a15a97fbeab75641f595c4
dhywty <clemwek@gmail.com>
2018-01-21 16:19:20 +0300

    setup redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import Routes from './routes';
@@ -7,7 +9,9 @@ import Routes from './routes';
 injectTapEventPlugin();
 
 export default () => (
-  <MuiThemeProvider >
-    <Routes />
-  </MuiThemeProvider >
+  <Provider store={store}>
+    <MuiThemeProvider >
+      <Routes />
+    </MuiThemeProvider >
+  </Provider>
 );
